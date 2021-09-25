0babaa430849f6a47960cc85653546e1e6757aad
Cristian Paolini <cristianpaolini3@gmail.com>
2021-09-25 02:09:39 -0300

    Added methods to delete incomes and/or expenses.

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@ const ingresos = [
 ];
 
 const egresos = [
-    new Egreso('Renta departamento', 900),
-    new Egreso('Ropa', 400)
+    new Egreso('Seguro auto', 4000),
+    new Egreso('Placa de video', 25000)
 ];
 
 let cargarApp = () => {
     cargarCabecero();
     cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = () => {
@@ -54,6 +55,14 @@ const cargarIngresos = () => {
     document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
 }
 
+const cargarEgresos = () => {
+    let egresosHTML = '';
+    for(let egreso of egresos) {
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+}
+
 const crearIngresoHTML = (ingreso) => {
     let ingresoHTML = `
     <div class="elemento limpiarEstilos">
@@ -62,11 +71,46 @@ const crearIngresoHTML = (ingreso) => {
         <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
         <div class="elemento_eliminar">
             <button class="elemento_eliminar--btn">
-                <ion-icon name="close-circle-outline"></ion-icon>
+                <ion-icon name="close-circle-outline"
+                onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
             </button>
         </div>
     </div>
 </div>
 `;
     return ingresoHTML;
+}
+
+const eliminarIngreso = (id) => {
+    let indexElim = ingresos.findIndex(ingreso => ingreso.id === id);
+    ingresos.splice(indexElim, 1);
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+}
+
+const crearEgresoHTML = (egreso) => {
+    let egresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"
+                onclick="eliminarEgreso(${egreso.id})"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+`;
+    return egresoHTML;
+}
+
+const eliminarEgreso = (id) => {
+    let indexElim = egresos.findIndex(egreso => egreso.id === id);
+    egresos.splice(indexElim, 1);
+    cargarCabecero();
+    cargarEgresos();
 }
